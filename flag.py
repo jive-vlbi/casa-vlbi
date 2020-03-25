90b94fc749de3f9319f2081cf1dc3e717732ec6e
Mark Kettenis <kettenis@jive.eu>
2020-03-25 14:43:22 +0100

    Import inspect module

--- flag.py
+++ flag.py
@@ -17,6 +17,7 @@
 # along with this library; if not, write to the Free Software Foundation,
 # Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
 #
+import inspect
 import optparse
 import datetime
 import sys
