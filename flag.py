f4fd9933cda549a683f5b2e7044fcf90e488168a
Mark Kettenis <kettenis@jive.eu>
2020-04-06 13:40:37 +0200

    Make it possible to use this as a module

--- flag.py
+++ flag.py
@@ -39,52 +39,67 @@ def find_first_dobs(idifiles):
             first_dobs = dobs
     return first_dobs
 
-# Allow this scrupt to be invoked using casa -c
-try:
-    i = sys.argv.index("-c") + 2
-except:
-    i = 1
-    pass
-
-usage = "usage %prog [options] uvflagfile fitsfile"
-parser = optparse.OptionParser(usage=usage)
-(options, args) = parser.parse_args(sys.argv[i:])
-
-first_date = find_first_dobs(args[1:])
-
-fp = open(args[0], 'r')
-flags = key.read_keyfile(fp)
-fp.close()
-
-flags = [dict(x) for x in flags]
-
-for flag in flags:
-    antenna = flag['ANT_NAME'].upper()
-    timerange = flag['TIMERANG']
-    if timerange == [0.0, 0.0, 0.0, 0.0, 400.0, 0.0, 0.0, 0.0]:
-        timerange = ""
-    else:
-        year = datetime.datetime(first_date.year, 1, 1)
-        date1 = year + datetime.timedelta(timerange[0] - 1)
-        date1 = date1.strftime("%Y/%m/%d")
-        date2 = year + datetime.timedelta(timerange[4] - 1)
-        date2 = date2.strftime("%Y/%m/%d")
-        timerange = "%s/%02d:%02d:%02d~%s/%02d:%02d:%02d" % \
-            (date1, timerange[1], timerange[2], timerange[3],
-             date2, timerange[5], timerange[6], timerange[7])
+def convert_flags(infile, idifiles, outfp=sys.stdout, outfile=None):
+    # Make sure we're dealing with a list
+    if not isinstance(idifiles, list):
+        idifiles = [idifiles]
         pass
-    if 'BIF' in flag:
-        spw = "%d~%d" % (flag['BIF'] -1, flag['EIF'] - 1)
-    else:
-        spw = ""
+
+    # Create new output file if requested to do so
+    if outfile:
+        outfp = open(outfile, 'w')
         pass
-    if 'BCHAN' in flag:
-        spw = spw + ":%d~%d" % (flag['BCHAN']-1, flag['ECHAN']-1)
+
+    first_date = find_first_dobs(idifiles)
+
+    infp = open(infile, 'r')
+    flags = key.read_keyfile(infp)
+    infp.close()
+
+    flags = [dict(x) for x in flags]
+
+    for flag in flags:
+        antenna = flag['ANT_NAME'].upper()
+        timerange = flag['TIMERANG']
+        if timerange == [0.0, 0.0, 0.0, 0.0, 400.0, 0.0, 0.0, 0.0]:
+            timerange = ""
+        else:
+            year = datetime.datetime(first_date.year, 1, 1)
+            date1 = year + datetime.timedelta(timerange[0] - 1)
+            date1 = date1.strftime("%Y/%m/%d")
+            date2 = year + datetime.timedelta(timerange[4] - 1)
+            date2 = date2.strftime("%Y/%m/%d")
+            timerange = "%s/%02d:%02d:%02d~%s/%02d:%02d:%02d" % \
+                (date1, timerange[1], timerange[2], timerange[3],
+                 date2, timerange[5], timerange[6], timerange[7])
+            pass
+        if 'BIF' in flag:
+            spw = "%d~%d" % (flag['BIF'] -1, flag['EIF'] - 1)
+        else:
+            spw = ""
+            pass
+        if 'BCHAN' in flag:
+            spw = spw + ":%d~%d" % (flag['BCHAN']-1, flag['ECHAN']-1)
+            pass
+        cmd = "antenna='%s'" % antenna
+        if timerange:
+            cmd = cmd + " timerange='%s'" % timerange
+            pass
+        if spw:
+            cmd = cmd + " spw='%s'" % spw
+            pass
+        outfp.write(cmd + "\n")
+    return
+
+if __name__ == "__main__":
+    # Allow this scrupt to be invoked using casa -c
+    try:
+        i = sys.argv.index("-c") + 2
+    except:
+        i = 1
         pass
-    cmd = "antenna='%s'" % antenna
-    if timerange:
-        cmd = cmd + " timerange='%s'" % timerange
-    if spw:
-        cmd = cmd + " spw='%s'" % spw
-        pass
-    print cmd
+
+    usage = "usage %prog [options] uvflagfile fitsfile"
+    parser = optparse.OptionParser(usage=usage)
+    (options, args) = parser.parse_args(sys.argv[i:])
+    convert_flags(args[0], args[1:])
